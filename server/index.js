3e4cd589274b945b80c456f1f77be176a664a06d
Richard Oh <sioh89@gmail.com>
2017-11-08 15:03:37 -0800

    no extra changes from last

--- server/index.js
+++ server/index.js
@@ -16,5 +16,10 @@ app.use(express.static(path.resolve(__dirname, '../client/public')));
 
 app.listen(PORT, function(err) {
   if(err) { throw err }
+<<<<<<< HEAD
 })
 
+=======
+  console.log('listening on port ', PORT);
+})
+>>>>>>> [ops]
