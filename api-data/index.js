6d48143e5ea69e480e1790fc2e2158d01f76e765
Richard Oh <sioh89@gmail.com>
2017-11-08 20:12:10 -0800

    [ops] -Cannot save to database from client side duh. Do a post request to server

--- api-data/index.js
+++ api-data/index.js
@@ -5,17 +5,7 @@ exports.getAllPatients = patient.getAllPatients;
 exports.getPatientById = patient.getPatientById; 
 exports.queryPatients = patient.queryPatients; 
 
-<<<<<<< HEAD
 exports.getAllMedicationOrders = order.getAllMedicationOrders;
 exports.getMedicationOrderById = order.getMedicationOrderById;
 exports.getMedicationOrdersByPatientId = order.getMedicationOrdersByPatientId;
 exports.queryMedicationOrders = order.queryMedicationOrders;
-=======
-
-// patient.getAllPatients((err, data) => console.log('Number of Patients:', data.length));
-
-// var filter = (patient => patient.gender ==='female');
-// patient.queryPatients(filter, (err, data) => console.log('Female Patients:', data.length));
-
-// order.getAllMedicationOrders((err, data) => console.log('Number of Medication Orders:', data.length));
->>>>>>> [obj]
--- client/src/actions/saveNoteAction.js
+++ client/src/actions/saveNoteAction.js
@@ -1,14 +1,7 @@
-// let sequelize = require('../../../db/index.js');
-// import sequelize from '../../../db/index.js';
-
 export const saveNote = noteText => {
   console.log(`You are trying to save note: ${noteText}`);
-  // sequelize.models.note.create({id:1, note:noteText}).then(() => {
-  //   sequelize.model.note.findAll({where: {}}).then(allNotes => {
-      return {
-        type: "SAVE_NOTE",
-        payload: allNotes
-      }
-  //   })
-  // })
+    return {
+      type: "SAVE_NOTE",
+      payload: noteText
+    }
 }
