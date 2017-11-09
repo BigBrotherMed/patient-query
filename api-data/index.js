9ee456101343824ea43d1d07d94e024d2617a8c3
Richard Oh <sioh89@gmail.com>
2017-11-08 16:59:31 -0800

    Created basic patientDetails components

--- api-data/index.js
+++ api-data/index.js
@@ -1,18 +1,11 @@
 const patient = require('./patient.js');
+const order = require('./medication-order.js');
 
 exports.getAllPatients = patient.getAllPatients; 
 exports.getPatientById = patient.getPatientById; 
 exports.queryPatients = patient.queryPatients; 
 
-<<<<<<< HEAD
 exports.getAllMedicationOrders = order.getAllMedicationOrders;
 exports.getMedicationOrderById = order.getMedicationOrderById;
 exports.getMedicationOrdersByPatientId = order.getMedicationOrdersByPatientId;
 exports.queryMedicationOrders = order.queryMedicationOrders;
-=======
-
-patient.getAllPatients((err, data) => console.log('Number of Patients:', data.length));
-
-var filter = (patient => patient.gender ==='female');
-patient.queryPatients(filter, (err, data) => console.log('Female Patients:', data.length));
->>>>>>> Created basic patientDetails components
