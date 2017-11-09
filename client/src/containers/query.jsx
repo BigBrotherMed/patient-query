2136d8bee516f27d94e5ff12aecda054cd93ef78
Kai An David Lee <david.ka.lee@gmail.com>
2017-11-08 17:16:48 -0800

    Added firstName field for query.

--- client/src/containers/query.jsx
+++ client/src/containers/query.jsx
@@ -1,9 +1,40 @@
 import React from 'react';
 
-const Query = () => (
-  <div>
-    <p>Query component goes here</p>
-  </div>
-);
+class Query extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      firstName: ''
+    }
+    this.handleFirstNameChange = this.handleFirstNameChange.bind(this);
+  }
+
+  handleFirstNameChange(event) {
+    this.setState({firstName: event.target.value});
+  }
+  
+
+  render() {
+    return (
+      <div>
+        <input value={this.state.firstName} onChange={this.handleFirstNameChange} placeholder="First Name" />
+        <button> Query </button>
+
+
+
+      </div>
+
+
+
+    )
+  }
+}
+
+// const Query = () => (
+//   <div>
+//     <p>Query component goes here</p>
+//   </div>
+// );
 
 export default Query;
