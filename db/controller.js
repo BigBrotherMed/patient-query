40ec272118872db5cf447d1e3a1aafba9a3bc3b8
Richard Oh <sioh89@gmail.com>
2017-11-09 09:46:41 -0800

    [ops] -Helper functions are done

--- db/controller.js
+++ db/controller.js
@@ -1,5 +1,21 @@
 const sequelize = require('./index.js');
 
-exports.addNote = (patientId, note) => {
-  sequelize.models.note.create();
+//This function takes in the patient ID and a note to add. Callback parameter is
+//invoked on success with all notes associated with current patient
+exports.addNote = (patientId, note, callback) => {
+  sequelize.models.note.create({patientId:patientId, note:note})
+  .then(() => {
+    sequelize.models.note.findAll({where: {patientId:patientId}})
+    .done(allNotes => {
+      callback(allNotes);
+    })
+  });
+}
+
+//Callback is invoked on success
+exports.getNotes = (patientId, callback) => {
+  sequelize.models.note.findAll({where: {patientId:patientId}}).
+  done(allNotes => {
+    callback(allNotes);
+  })
 }
