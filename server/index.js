d3825cb57bf1ae61ba9a97e80f63af0f27a0074b
Brent Eickhoff <dev@eickhoffs.net>
2017-11-08 17:22:53 -0800

    Server gets WIP commented out, syncing with team

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const path = require('path');
 const parser = require('body-parser');
 const request = require('request');
-const fhir = require('../api_data/index.js');
+const fhir = require('../api-data/index.js');
 
 const app = express();
 const PORT = 5000;
@@ -13,14 +13,16 @@ app.use(parser.json());
 app.use(parser.urlencoded({extended: true}));
 app.use(express.static(path.resolve(__dirname, '../client/public')));
 
+// app.get('/patients', (req, res) => {
+//   fhir.getAllPatients(err, patients) => {
+//   	console.log('server GET/patients, records:', patients.length);
+//   	res.status(200).send(JSON.stringify({patients: patients}))
+//   }
+// });
 
-app.listen(PORT, function(err) {
-  if(err) { throw err }
-<<<<<<< HEAD
-  console.log(`now listening to ${PORT}`);
-})
-=======
-  console.log('listening on port ', PORT);
-});
->>>>>>> 4538827041ab0ebc390f4663a308c6d8406bb603
-
+// app.get('/medication-orders', (req, res) => {
+//   fhir.getAllMedicationOrders(err, orders) => {
+//   	console.log('server GET/orders, records:', orders.length);
+//   	res.status(200).send(JSON.stringify({orders: orders}))
+//   }
+// });
