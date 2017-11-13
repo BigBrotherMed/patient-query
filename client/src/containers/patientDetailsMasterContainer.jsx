4fc19c814732331e1714a67b911aaa6f87f200ef
Brent Eickhoff <dev@eickhoffs.net>
2017-11-13 14:51:09 -0800

    Implement sort for patien list and case insensitive for search

--- client/src/containers/patientDetailsMasterContainer.jsx
+++ client/src/containers/patientDetailsMasterContainer.jsx
@@ -44,6 +44,7 @@ class PatientDetailsMasterContainer extends React.Component {
                 <ListGroupItem bsStyle="info">
                   
                     {this.props.activePatient ? 
+                      this.props.activePatient.patient.id.slice(6) + ' - ' +
                       this.props.activePatient.patient.lastName + ', ' + 
                       this.props.activePatient.patient.firstName 
                       : null}
--- client/src/reducers/reducer-query.js
+++ client/src/reducers/reducer-query.js
@@ -3,7 +3,7 @@ export default function(state=null, action) {
     case "SUBMIT_QUERY":
       let results =[];
       for(let i = 0; i < action.payload.patients.length; i++) {
-        if(action.payload.patients[i].lastName.includes(action.payload.query.lastName)) {
+        if(action.payload.patients[i].lastName.toUpperCase().includes(action.payload.query.lastName.toUpperCase())) {
           results.push(action.payload.patients[i]);
         }
       }
